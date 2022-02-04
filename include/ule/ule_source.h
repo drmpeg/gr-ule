c80ffca03aed456c8a13bb282e403934e994f7ce
Ron Economos <w6rz@comcast.net>
2022-02-03 20:34:32 -0800

    Update for GNU Radio 3.9

--- include/ule/ule_source.h
+++ include/ule/ule_source.h
@@ -36,7 +36,7 @@ namespace gr {
     class ULE_API ule_source : virtual public gr::sync_block
     {
      public:
-      typedef boost::shared_ptr<ule_source> sptr;
+      typedef std::shared_ptr<ule_source> sptr;
 
       /*!
        * \brief Return a shared_ptr to a new instance of ule::ule_source.
